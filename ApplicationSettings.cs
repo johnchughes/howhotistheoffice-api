877760a7b7f6cf0b9297e961640e440e8c36c5f2
John Hughes <j.c.hughes7@gmail.com>
2022-07-24 11:02:03 +0100

    app settings refactor mk 1

---
+++ ApplicationSettings.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace server
+{
+
+    public interface IApplicationSettings
+    {
+        string STORAGE_ACCOUNT_NAME { get; } 
+        string STORAGE_ACCOUNT_KEY { get; }
+        string TABLE_URL_FORMAT { get; }
+    }
+
+    public class ApplicationSettings : IApplicationSettings
+    {   
+        public string STORAGE_ACCOUNT_NAME {get; private set;}
+        public string STORAGE_ACCOUNT_KEY {get; private set;}
+        public string TABLE_URL_FORMAT {get; private set;}
+
+        public ApplicationSettings(){
+            STORAGE_ACCOUNT_NAME = Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_NAME");
+            STORAGE_ACCOUNT_KEY = Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_KEY");
+            TABLE_URL_FORMAT = "https://{0}.table.core.windows.net/{1}";
+        }
+
+ 
+    }
+}
--- temps.cs
+++ temps.cs
@@ -15,19 +15,16 @@ namespace server
 {
     public class temps
     {
-        private readonly string TABLE_URL = "https://fntemps.table.core.windows.net/temp";
-        private readonly string TABLE_NAME = "temp";
-        private readonly string TABLE_ACCOUNT_NAME = "fntemps";
-        private readonly string TABLE_ACCOUNT_KEY = "crtflSMKFnKWIhJglDTq3e/HUCvezkUFD/zyYiAwoNY5PH8XgsF0c9Wvogfec1c55NXMLmi2HCPo+AStMHQyHg==";
+        private const string TABLE_NAME = "temp";
         private readonly string LATEST_PARTITION = "latest";
         private readonly string LATEST_ROWKEY = "now";
-        //partition by: Date
-        // row key: timestamp
+        private readonly string TABLE_URL;
 
-        //partiion: latest:
-        //row key: NOW (current displayed value)
+        IApplicationSettings appSettings = new ApplicationSettings();
 
-        //when we get an update shit from now to the logs, update now. 
+        public temps() {
+            TABLE_URL = string.Format(appSettings.TABLE_URL_FORMAT, appSettings.STORAGE_ACCOUNT_NAME, TABLE_NAME);
+        }
 
         [FunctionName("temps")]
         public async Task<IActionResult> Run(
@@ -35,19 +32,7 @@ namespace server
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            // string name = req.Query["name"];
-
-            // string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            // dynamic data = JsonConvert.DeserializeObject(requestBody);
-            // name = name ?? data?.name;
-
-            // string responseMessage = string.IsNullOrEmpty(name)
-            //     ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //     : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
             var current = GetLatest();
-
             return new OkObjectResult(current);
         }
 
@@ -66,7 +51,7 @@ namespace server
             tempRecord.RowKey = LATEST_ROWKEY;
             tempRecord.Timestamp = DateTimeOffset.Now;
             tempRecord.ETag = ETag.All;
-            var tableClient = new TableClient(new Uri(TABLE_URL), TABLE_NAME, new TableSharedKeyCredential(TABLE_ACCOUNT_NAME, TABLE_ACCOUNT_KEY));
+            var tableClient = new TableClient(new Uri(TABLE_URL), TABLE_NAME, new TableSharedKeyCredential(appSettings.STORAGE_ACCOUNT_NAME, appSettings.STORAGE_ACCOUNT_KEY));
             tableClient.AddEntity(tempRecord);
 
             log.LogInformation($"Temp: {tempRecord.Temperature} Time: {tempRecord.TemperatureTimeStamp}");
@@ -107,7 +92,7 @@ namespace server
             {
                 string date = req.Query["date"];
 
-                var tableClient = new TableClient(new Uri(TABLE_URL), TABLE_NAME, new TableSharedKeyCredential(TABLE_ACCOUNT_NAME, TABLE_ACCOUNT_KEY));
+                var tableClient = new TableClient(new Uri(TABLE_URL), TABLE_NAME, new TableSharedKeyCredential(appSettings.STORAGE_ACCOUNT_NAME, appSettings.STORAGE_ACCOUNT_KEY));
                 var queryResult = tableClient.Query<TemperatureRecord>(filter: $"PartitionKey eq '{date}'").ToList();
 
                 //var chartData = queryResult.Select(x => new ChartData() { Time = x.TemperatureTimeStamp, Temperature = x.Temperature}).OrderBy(x => x.Time);
@@ -121,7 +106,7 @@ namespace server
 
         private void UpdateLatest(TemperatureRecord temperatureReading)
         {
-            var tableClient = new TableClient(new Uri(TABLE_URL), TABLE_NAME, new TableSharedKeyCredential(TABLE_ACCOUNT_NAME, TABLE_ACCOUNT_KEY));
+            var tableClient = new TableClient(new Uri(TABLE_URL), TABLE_NAME, new TableSharedKeyCredential(appSettings.STORAGE_ACCOUNT_NAME, appSettings.STORAGE_ACCOUNT_KEY));
             TemperatureRecord currentRecord = tableClient.GetEntity<TemperatureRecord>(LATEST_PARTITION, LATEST_ROWKEY);
             currentRecord.Temperature = temperatureReading.Temperature;
             currentRecord.TemperatureTimeStamp = temperatureReading.TemperatureTimeStamp;
@@ -142,13 +127,13 @@ namespace server
             data.RowKey = rowKey;
             data.Timestamp = DateTimeOffset.Now;
             data.ETag = ETag.All;
-            var tableClient = new TableClient(new Uri(TABLE_URL), TABLE_NAME, new TableSharedKeyCredential(TABLE_ACCOUNT_NAME, TABLE_ACCOUNT_KEY));
+            var tableClient = new TableClient(new Uri(TABLE_URL), TABLE_NAME, new TableSharedKeyCredential(appSettings.STORAGE_ACCOUNT_NAME, appSettings.STORAGE_ACCOUNT_KEY));
             tableClient.AddEntity(data);
         }
 
         private TemperatureRecord GetLatest()
         {
-            var tableClient = new TableClient(new Uri(TABLE_URL), TABLE_NAME, new TableSharedKeyCredential(TABLE_ACCOUNT_NAME, TABLE_ACCOUNT_KEY));
+            var tableClient = new TableClient(new Uri(TABLE_URL), TABLE_NAME, new TableSharedKeyCredential(appSettings.STORAGE_ACCOUNT_NAME, appSettings.STORAGE_ACCOUNT_KEY));
             var queryResult = tableClient.Query<TemperatureRecord>(filter: $"PartitionKey eq '{LATEST_PARTITION}' and RowKey eq '{LATEST_ROWKEY}'").Single();
             return queryResult;
         }
